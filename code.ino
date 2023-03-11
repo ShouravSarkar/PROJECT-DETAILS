794b457c713435f989fd5d22506c376778bd9dd8
ShouravSarkar <65628400+ShouravSarkar@users.noreply.github.com>
2023-03-11 17:06:11 +0600

    Create code.ino
    arduino code for atmega168

---
+++ code.ino
@@ -0,0 +1,9 @@
+In fiverr I can't share my address and details. as it is not supported. 
+
+here is my email : shouravEEE015@gmail.com
+whats app : +8801783350821
+
+you have to bear the cost of tax and other. we can continue the discussion in whats app or mail also . and for payment as fiverr cut almost 20% from both 
+of us . we can do that outside also. it is your choise. 
+
+please send me a mail or text me in whats app
