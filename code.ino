86ff207f2351b1d437c34b3e3892c12bc969a65a
ShouravSarkar <65628400+ShouravSarkar@users.noreply.github.com>
2023-03-11 23:59:02 +0600

    delete

--- code.ino
+++
@@ -1,9 +0,0 @@
-In fiverr I can't share my address and details. as it is not supported. 
-
-here is my email : shouravEEE015@gmail.com
-whats app : +8801783350821
-
-you have to bear the cost of tax and other. we can continue the discussion in whats app or mail also . and for payment as fiverr cut almost 20% from both 
-of us . we can do that outside also. it is your choise. 
-
-please send me a mail or text me in whats app
