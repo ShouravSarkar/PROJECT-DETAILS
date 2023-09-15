fde79166bb7739c30c267cc60a6091744c170ab9
ShouravSarkar <65628400+ShouravSarkar@users.noreply.github.com>
2023-09-15 15:08:07 +0200

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,4 +1,37 @@
-I might not be available via fiverr for some days. 
-it will be better if you can text me in whats app: +8801783350821 I will give you new files and updates there and you can check the files and update me about it in what's app.
+/*
+  Blink
 
-and the cost will be 30 USD. I am trying to keep that as low as possible as there is some miscommunication. let me know if it is okay.
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);                      // wait for a second
+}
