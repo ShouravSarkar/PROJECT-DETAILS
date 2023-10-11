56b6dff2f3aae5ad45a58cc5ad3683887ee004f0
ShouravSarkar <65628400+ShouravSarkar@users.noreply.github.com>
2023-10-12 01:49:41 +0200

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,3 +1,3 @@
-please text me in whats app : +8801783350821 
+please text me in whats app : +8801783350821 or email: shouraveee015@gmail.com
 
 fiverr charge me almost 20% lot so it will be better if we can make most of the payment outside and a small amount in fiverr. if you are okay with it then It will save a me a lot.
