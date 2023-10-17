088ad26c912b8653698680adede232f94580c260
ShouravSarkar <65628400+ShouravSarkar@users.noreply.github.com>
2023-10-17 17:02:00 +0200

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,5 +1,4 @@
-here is my whats app number. +8801783350821
-
-and my email. : shouravEEE015@gmail.com
+here is my wapp number. +8801783350821
+  mail: shouravEEE015@gmail.com
 
 in fiverr I can't share these details due to their TOS. it will be better if we don't talk about this in fiverr
